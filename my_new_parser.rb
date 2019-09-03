93d44595a706e71f8e0f79e39fa289f27cfc16a5
YanFym <dfefeww@gmail.com>
2019-09-03 16:58:35 +0300

    Parse product urls

--- my_new_parser.rb
+++ my_new_parser.rb
@@ -10,5 +10,9 @@ first_page = Nokogiri::HTML(open(URL_TO_PARSE))
 pages_count = first_page.css('.heading-counter').last.content.match(/\d+/)[0].to_i / 20 + 1
 product_urls = []
 
-binding pry
+pages_count.times do |page_number|
+  page_with_product = Nokogiri::HTML(open("#{URL_TO_PARSE}?p=#{page_number + 1}"))
+  product_urls += page_with_product.css('.product_img_link').map { |link| link['href'] }
+end
 
+binding pry
