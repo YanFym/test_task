39da85b4b8b7cd6f6541eeb119d42fd6c5038740
YanFym <dfefeww@gmail.com>
2019-09-03 17:12:28 +0300

    fetch all params

--- my_new_parser.rb
+++ my_new_parser.rb
@@ -4,6 +4,20 @@ require 'open-uri'
 require 'pry'
 
 URL_TO_PARSE = ARGV.first
+FILE_TO_SAVE= ARGV.last
+
+def parse_product(product_url)
+  page = Nokogiri::HTML(open(product_url))
+  image_src = page.css('#bigpic').last['src']
+  product_name = page.css('h1').last.content.strip
+  page.css('.attribute_labels_lists').map do |line|
+    [
+      "#{product_name} -- #{line.css('.attribute_name').last.content.strip}",
+      line.css('.attribute_price').last.content.strip,
+      image_src
+    ]
+  end
+end
 
 #binding pry
 first_page = Nokogiri::HTML(open(URL_TO_PARSE))
@@ -13,6 +27,10 @@ product_urls = []
 pages_count.times do |page_number|
   page_with_product = Nokogiri::HTML(open("#{URL_TO_PARSE}?p=#{page_number + 1}"))
   product_urls += page_with_product.css('.product_img_link').map { |link| link['href'] }
+end
+
+product_lines = product_urls.inject([]) do |product_lines, product_url|
+  product_lines + parse_product(product_url)
 end
 
 binding pry
