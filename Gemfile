591a40236cbd512df23f7654fb4d740d5da19051
YanFym <dfefeww@gmail.com>
2019-09-10 19:51:38 +0300

    Add curb

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
 
 gem "nokogiri", ">= 1.10.4"
+gem 'curb', '~> 0.9.3'
 gem 'pry'
--- my_new_parser.rb
+++ my_new_parser.rb
@@ -2,12 +2,13 @@ require 'nokogiri'
 require 'csv'
 require 'open-uri'
 require 'pry'
+require 'curb'
 
 ALL_INFORMATION = ARGV.first
 END_CSV_FILE = ARGV.last
 
 def parse_product(product_url)
-  page = Nokogiri::HTML(open(product_url))
+  page = Nokogiri::HTML(Curl.get(product_url).body_str)
   image_src = page.xpath('//div/div/div/div/div/span/img/@src').text
   product_name = page.xpath('//h1').text
   page.css('.attribute_labels_lists').map do |line|
@@ -19,12 +20,12 @@ def parse_product(product_url)
   end
 end
 
-first_page = Nokogiri::HTML(open(ALL_INFORMATION))
-pages_count = pages_count = first_page.xpath('//div[2]/div/div/div/div[1]/div[2]/span').text.match(/\d+/)[0].to_i / 20 + 1
+first_page = Nokogiri::HTML(Curl.get(ALL_INFORMATION).body_str)
+pages_count = first_page.xpath('//div[2]/div/div/div/div[1]/div[2]/span').text.match(/\d+/)[0].to_i / 20 + 1
 product_urls = []
 
 pages_count.times do |page_number|
-  page = Nokogiri::HTML(open("#{ALL_INFORMATION}?p=#{page_number + 1}"))
+  page = Nokogiri::HTML(Curl.get("#{ALL_INFORMATION}?p=#{page_number + 1}").body_str)
   product_urls += page.css('.product_img_link').map { |link| link['href'] }
 end
 
