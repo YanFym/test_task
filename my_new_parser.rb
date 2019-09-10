c5f0e8d03f4e007a7c489f574f02ec6f7e90ecf6
YanFym <dfefeww@gmail.com>
2019-09-10 22:06:36 +0300

    Now with OOP

--- my_new_parser.rb
+++ my_new_parser.rb
@@ -7,35 +7,52 @@ require 'curb'
 ALL_INFORMATION = ARGV.first
 END_CSV_FILE = ARGV.last
 
-def parse_product(product_url)
-  page = Nokogiri::HTML(Curl.get(product_url).body_str)
-  image_src = page.xpath('//div/div/div/div/div/span/img/@src').text
-  product_name = page.xpath('//h1').text
-  page.css('.attribute_labels_lists').map do |line|
-    [
-      "#{product_name} -- #{line.css('.attribute_name').last.content.strip}",
-      line.css('.attribute_price').last.content.strip,
-      image_src
-    ]
+class MyNewParser
+  def initialize
+    write_to_csv_file
+  end
+
+  def fetch_pages_count
+    first_page = Nokogiri::HTML(Curl.get(ALL_INFORMATION).body_str)
+    first_page.xpath('//div[2]/div/div/div/div[1]/div[2]/span').text.match(/\d+/)[0].to_i / 20 + 1  
+  end
+
+  def fetch_product_urls
+    product_urls = []
+    fetch_pages_count.times do |page_number|
+      page = Nokogiri::HTML(Curl.get("#{ALL_INFORMATION}?p=#{page_number + 1}").body_str)
+      product_urls += page.css('.product_img_link').map { |link| link['href'] }
+    end 
+    product_urls
+  end
+
+  def parse_product(product_url)
+    page = Nokogiri::HTML(Curl.get(product_url).body_str)
+    image_src = page.xpath('//div/div/div/div/div/span/img/@src').text
+    product_name = page.xpath('//h1').text
+    page.css('.attribute_labels_lists').map do |line|
+      [
+        "#{product_name} -- #{line.css('.attribute_name').last.content.strip}",
+        line.css('.attribute_price').last.content.strip,
+        image_src
+      ]
+    end
+  end
+
+  def fetch_product_lines
+    product_lines = fetch_product_urls.inject([]) do |product_lines, product_url|
+      product_lines + parse_product(product_url)
+    end   
+  end
+
+  def write_to_csv_file
+    CSV.open("tmp/#{END_CSV_FILE}", 'wb') do |csv|
+      csv << %w(Names Prices Images)
+      fetch_product_lines.each do |line|
+        csv << line
+      end
+    end
   end
 end
 
-first_page = Nokogiri::HTML(Curl.get(ALL_INFORMATION).body_str)
-pages_count = first_page.xpath('//div[2]/div/div/div/div[1]/div[2]/span').text.match(/\d+/)[0].to_i / 20 + 1
-product_urls = []
-
-pages_count.times do |page_number|
-  page = Nokogiri::HTML(Curl.get("#{ALL_INFORMATION}?p=#{page_number + 1}").body_str)
-  product_urls += page.css('.product_img_link').map { |link| link['href'] }
-end
-
-product_lines = product_urls.inject([]) do |product_lines, product_url|
-  product_lines + parse_product(product_url)
-end
-
-CSV.open("tmp/#{END_CSV_FILE}", 'wb') do |csv|
-  csv << %w(Names Prices Images)
-  product_lines.each do |line|
-    csv << line
-  end
-end
+MyNewParser.new
